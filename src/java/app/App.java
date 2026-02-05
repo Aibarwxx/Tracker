9ce70e94c687a697c890b2e071e65977ba8af511
Aibar <ajbarmadiuly@gmail.com>
2026-02-05 13:57:34 +0500

    feat! Added basic structure for implementing ui

---
+++ src/java/app/App.java
@@ -0,0 +1,8 @@
+package app;
+import ui.*;
+
+public class App{
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/java/ui/MainMenu.java
@@ -0,0 +1,9 @@
+package ui;
+import javax.swing.*;
+import java.io.*;
+
+public class MainMenu{
+    public static void launchMenu(){
+
+    }
+}
