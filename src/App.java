d74765400b64ba9cd47c8dea67a154ce059a95d3
Aibar <ajbarmadiuly@gmail.com>
2026-02-07 23:22:53 +0500

    creating to do list

--- src/App.java
+++ src/App.java
@@ -1,7 +1,12 @@
-package app;
+import ui.MainMenu;
 
 public class App{
     public static void main(String[] args){
         MainMenu.launchMenu();
     }
 }
+
+// TODO
+// create entities
+// create DB manipulation class
+// implement Main Menu with functional inside (account information)
